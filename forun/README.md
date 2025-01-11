779663ee18ef846289e6e90ea0a6acb3045a13f3
FisherSkyi <yu.letian66@gmail.com>
2025-01-11 21:15:18 +0800

    set up controller for index and new

--- forun/README.md
+++ forun/README.md
@@ -1,12 +1,7 @@
 <<<<<<< HEAD
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
+* Ruby version: ruby 3.4.1
 
 * System dependencies
 
@@ -22,5 +17,5 @@ Things you may want to cover:
 
 * Deployment instructions
 
-* ...
+
 
--- forun/app/controllers/posts_controller.rb
+++ forun/app/controllers/posts_controller.rb
@@ -1,22 +1,50 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user = current_user # Assuming you have authentication
+    if @post.save
+      redirect_to @post, notice: "Post created successfully."
+    else
+      render :new, alert: "Failed to create post."
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post updated successfully."
+    else
+      render :edit, alert: "Failed to update post."
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "Post deleted successfully."
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
