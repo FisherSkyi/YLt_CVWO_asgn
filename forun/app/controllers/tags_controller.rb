7b1df45ac00fb8cfe076d569b0782c7e82ad0d23
FisherSkyi <yu.letian66@gmail.com>
2025-01-13 11:25:11 +0800

    add feature: find all posts with same tag

--- forun/app/controllers/tags_controller.rb
+++ forun/app/controllers/tags_controller.rb
@@ -1,8 +1,11 @@
 class TagsController < ApplicationController
   def index
+    @tags = Tag.all
   end
 
   def show
+    @tag = Tag.find(params[:id])
+    @posts = @tag.posts.includes(:user, :tags)
   end
 
   def create
--- forun/config/routes.rb
+++ forun/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :posts
   # Home page redirects to the posts index
   root "posts#index"
 
@@ -12,7 +11,7 @@ Rails.application.routes.draw do
   # Post routes (with nested comments)
   resources :posts do
     resources :comments, only: [:create, :destroy] # Comments nested under posts
-    resources :tags, only: [:index, :create], shallow: true # Tags nested under posts
+    # resources :tags, only: [:index, :create], shallow: true # Tags nested under posts
   end
 
   # Tag routes (view posts by a specific tag)
